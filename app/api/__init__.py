d2de26085f28549ee0ef0e648d904bc546d39484
FelipeRibeiro12 <felipe.gabriel@ges.inatel.br>
2026-04-10 11:34:24 -0300

    feat: adiciona rotas de dieta na API

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,1 @@
-from fastapi import APIRouter
-from app.api.endpoints.user import router as user_router
-
-api_router = APIRouter()
-api_router.include_router(user_router, prefix="/users", tags=["users"])
+from .api import api_router
