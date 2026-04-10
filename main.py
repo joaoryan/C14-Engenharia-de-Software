c8e278e4035cef088b8825e80b8f9e56fc7e2465
joaoryan <sjoaoryan@gmail.com>
2026-04-10 09:55:01 -0300

    feat: implementa rotas da API de usuario

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from app.api import api_router
+from app.db.base import Base
+from app.db.session import engine
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"status": "Sistema de Dieta Ativo"}
+    return {"message": "Bem-vindo à API do Diet"}
+
+app.include_router(api_router, prefix="/api")
+
+Base.metadata.create_all(bind=engine)
